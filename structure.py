e0116447fd6564b81caf4024acd000357155aee2
Sai Santosh Pal <78339151+Sai-Santosh-Pal@users.noreply.github.com>
2023-07-26 12:39:53 +0530

    Update structure.py

--- structure.py
+++ structure.py
@@ -50,3 +50,64 @@ print(g.latlng)
 
 g = geocoder.ip('me')
 g.address
+
+# absa
+# from geopy.geocoders import Nominatim
+# loc = Nominatim(user_agent="GetLoc")
+
+# class Location:
+#     def __init__(self):
+#         pass
+#     def find_coords(self):
+#         try:
+#             return [getLoc.longitude, getLoc.longitude]
+#         except Exception as e:
+#             return e
+
+# loc = Location()
+
+# class ApiStructure:
+#     def __init__(self, location=loc.find_coords()):
+#         self.location = location
+#     def get_coords(self):
+#         print(self.location)
+#         return self.location
+
+# api = ApiStructure()
+# api.get_coords()
+# print("done")
+
+# from geopy.geocoders import Nominatim
+
+# def get_user_location():
+#     try:
+#         # Using 'ipinfo.io' as the geocoder
+#         geolocator = Nominatim(user_agent="geoapiExercises", format_string="%s")
+#         location = geolocator.geocode('')
+        
+#         # Extract location details
+#         if location:
+#             latitude = location.latitude
+#             longitude = location.longitude
+#             address = location.address
+#             return {
+#                 'latitude': latitude,
+#                 'longitude': longitude,
+#                 'address': address
+#             }
+#         else:
+#             return None
+
+#     except Exception as e:
+#         print("Error occurred:", e)
+#         return None
+
+# if __name__ == "__main__":
+#     user_location = get_user_location()
+#     if user_location:
+#         print("Latitude:", user_location['latitude'])
+#         print("Longitude:", user_location['longitude'])
+#         print("Address:", user_location['address'])
+#     else:
+#         print("Failed to retrieve the user's location.")
+
